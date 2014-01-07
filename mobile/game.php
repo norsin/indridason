23a2c80e3a14eeae34fa28fe68202fac46ee6cc4
norsin <nt@bilandia.de>
2014-01-07 10:14:59 +0100

    #7341 Add Content

--- mobile/game.php
+++ mobile/game.php
@@ -4,34 +4,33 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
-        <title>Schräg verliebt</title>
-        <meta name="description" content="description text" />
-        <link rel="image_src" href="">
-        <meta property="og:image" content=""/>
-        <meta property="og:image:secure_url" content="">
+        <title>Arnaldur Indriðason – Duell</title>
+        <link rel="image_src" href="img/share.jpg">
+        <meta property="og:image" content="http://f-bilandia.de/luebbe/indridason/img/share.jpg"/>
+        <meta property="og:image:secure_url" content="https://f-bilandia.de/luebbe/indridason/img/share.jpg">
         <meta property="og:image:type" content="image/jpg">
         <meta property="og:image:width" content="200">
         <meta property="og:image:height" content="200">
-        <meta property="og:title" content="og title"/>
-        <meta property="og:description" content="og description text" />
-        <meta property="og:url" content=""/>
+        <meta property="og:title" content="Arnaldur Indriðason – Duell"/>
+        <meta property="og:description" content="Fan werden und den neuen Roman von Krimiautor Arnaldur Indriðason gewinnen!" />
+        <meta property="og:url" content="http://f-bilandia.de/luebbe/indridason/weiche.php"/>
         <link rel="stylesheet" href="css/frameset.css">
         </head>	
 	<body class="home">
             <div class="wrapper">
                <div class="header">
  		    <h1 class="heading">Duell</h1>
-		    <h2 class="heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit</h2>
-                    <a href="start.php" class="losBtn button">Zu ersten Folge</a>
+		    <h2 class="heading">Der Beginn einer neuen Krimiserie vom erfolgreichsten Autor Islands</h2>
+                    <a href="start.php" class="losBtn button">Mehr zu "Duell"</a>
                 </div>
                 <div class="content">
 		    <h3 class="heading">Gewinnspiel</h3>
-		    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
-<p class="text highlight"><strong>Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae.</strong></p>
- <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
+		    <p class="text">In unserem Gewinnspiel verlosen wir je 5 Exemplare des Buches und des Hörbuches von "Duell". Dafür möchten wir von euch wisssen:</p>
+<p class="text highlight"><strong>Wie heißt der Ermittler in Arnaldur Indriðasons "Duell"?</strong></p>
+ <p class="text">Schickt uns die richtige Lösung per E-Mail und dem Betreff "Duell" bis zum 16.02.2014 an gewinnspiel@luebbe.de. Bitte gibt euren Namen und eure Postadresse an.</p>
                     <div class="btnRow">
-                        <a href="mailto:facebook@luebbe.de?subject=Duell" target="_blank" class="mitmachen button fullWidth">Mitmachen</a>
-			<a href="#" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
+                        <a href="mailto:gewinnspiel@luebbe.de?subject=Duell" target="_blank" class="mitmachen button fullWidth">Mitmachen</a>
+			<a href="https://www.facebook.com/BasteiLuebbe" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
                     </div>
                 </div>
 
--- mobile/start.php
+++ mobile/start.php
@@ -4,33 +4,32 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width">
-        <title>Schräg verliebt</title>
-        <meta name="description" content="description text" />
-        <link rel="image_src" href="">
-        <meta property="og:image" content=""/>
-        <meta property="og:image:secure_url" content="">
+        <title>Arnaldur Indriðason – Duell</title>
+        <link rel="image_src" href="img/share.jpg">
+        <meta property="og:image" content="http://f-bilandia.de/luebbe/indridason/img/share.jpg"/>
+        <meta property="og:image:secure_url" content="https://f-bilandia.de/luebbe/indridason/img/share.jpg">
         <meta property="og:image:type" content="image/jpg">
         <meta property="og:image:width" content="200">
         <meta property="og:image:height" content="200">
-        <meta property="og:title" content="og title"/>
-        <meta property="og:description" content="og description text" />
-        <meta property="og:url" content=""/>
+        <meta property="og:title" content="Arnaldur Indriðason – Duell"/>
+        <meta property="og:description" content="Fan werden und den neuen Roman von Krimiautor Arnaldur Indriðason gewinnen!" />
+        <meta property="og:url" content="http://f-bilandia.de/luebbe/indridason/weiche.php"/>
         <link rel="stylesheet" href="css/frameset.css">
         </head>	
 	<body class="home">
             <div class="wrapper">
                 <div class="header">
 		    <h1 class="heading">Duell</h1>
-		    <h2 class="heading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit</h2>
+		    <h2 class="heading">Der Beginn einer neuen Krimiserie vom erfolgreichsten Autor Islands</h2>
                     <a href="game.php" class="losBtn button">Zum Gewinnspiel</a>
                 </div>
                 <div class="content">
-                    <img class="bookCover" src="images/bookcover.png">
-		    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nisi id augue suscipit sagittis tincidunt non tellus. Suspendisse ac arcu eu nibh placerat molestie eu nec nisl. Nunc vitae mauris turpis. Etiam ut auctor magna. Quisque feugiat turpis lorem, ut feugiat nunc bibendum vitae. Praesent consectetur interdum massa eu ultricies. Suspendisse potenti. In hac habitasse platea dictumst</p>
+                    <img class="bookCover" src="img/bookcover.png">
+		    <p class="text">Reykjavík 1972, der russische Schachweltmeister Boris Spasski tritt mitten im Kalten Krieg gegen seinen amerikanischen Herausforderer Bobby Fischer an. In der aufgeheizten Stimmung wird ein Jugendlicher in einem Reykjavíker Kino brutal erschlangen.</p>
                     <div class="btnRow">
                         <a href="../pdf/leseprobe_duell.pdf" target="_blank" class="leseprobe button fullWidth">Leseprobe</a>
 			<a href="http://www.luebbe.de/Buecher/Spannung/Details/Id/978-3-7857-2483-5/?et_cid=77&et_lid=381200&et_sub=Indridason_Duell_Buch" target="_blank" class="mehr button fullWidth">Mehr zu &quot;Duell&quot;</a>
-			<a href="#" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
+			<a href="https://www.facebook.com/BasteiLuebbe" target="_blank" class="back button fullWidth">Zur&uuml;ck zu Facebook</a>
                     </div>
                 </div>
 
